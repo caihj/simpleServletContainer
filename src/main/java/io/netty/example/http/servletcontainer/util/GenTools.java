b7f2af7fd5a3857ad632f5aa02af28e3cb2ebc2d
ggworld <814130336@qq.com>
2017-09-14 23:03:40 +0800

    no fix

--- src/main/java/io/netty/example/http/servletcontainer/util/GenTools.java
+++ src/main/java/io/netty/example/http/servletcontainer/util/GenTools.java
@@ -3,6 +3,9 @@ package io.netty.example.http.servletcontainer.util;
 import java.util.Enumeration;
 import java.util.Iterator;
 
+/**
+ * 通用工具
+ */
 public class GenTools {
 
     public static Enumeration iteratorToEnumeration(final Iterator  iterable){
